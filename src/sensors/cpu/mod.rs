139f2b5640251f3e32fa161664e1705bd09fe7f3
nakal <nakal@users.noreply.github.com>
2017-05-30 19:16:38 +0200

    Retrieve cp_time mib on FreeBSD
    (untested)

--- src/sensors/cpu/mod.rs
+++ src/sensors/cpu/mod.rs
@@ -19,16 +19,31 @@ impl CPUInfo {
     }
 
     pub fn fetch() -> CPUInfo {
+        #[cfg(target_os = "openbsd")]
         let mut mib: [libc::c_int; 2] = [ libc::CTL_KERN, libc::KERN_CPTIME ];
+        #[cfg(target_os = "freebsd")]
+        let mut mib: [libc::c_int; 4] = CPUInfo::cp_time_mib();
         let mut info = CPUInfo::new();
         let buf: *mut c_void = info.state.as_mut_ptr() as *mut c_void;
         let mut size = mem::size_of::<CPUInfo>() as size_t;
         let outbuf: *mut c_void = ptr::null_mut() as *mut c_void;
         unsafe {
-            libc::sysctl(&mut mib[0], 2, buf,
+            libc::sysctl(&mut mib[0], mib.len() as u32, buf,
                          &mut size, outbuf, 0);
         }
         info
+    }
+
+    #[cfg(target_os = "freebsd")]
+    fn cp_time_mib() -> [libc::c_int; 4] {
+        let mut mib: [libc::c_int; 4] = [0, 0, 0, 0];
+        unsafe {
+            let mut size: usize = mib.len();
+            let sctlname = "kern.cp_time";
+            let name: *const i8 = sctlname.as_ptr() as *const i8;
+            libc::sysctlnametomib(name, &mut mib[0], &mut size);
+        }
+        mib
     }
 
     pub fn ncpus() -> u32 {
