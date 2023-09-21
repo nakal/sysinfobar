cf5f8b43391ba6d31e482ba6067141aa42a44bf4
nakal <nakal@users.noreply.github.com>
2023-09-21 18:05:32 +0200

    FontAwesome support is back
    It needs to be compiled with:
    
    ```
    cargo build --features fontawesome
    ```

--- README.md
+++ README.md
@@ -4,6 +4,18 @@
 information to stdout in a format that is expected to be piped to
 [Xmobar](http://projects.haskell.org/xmobar/).
 
+## Screenshots
+
+### FontAwesome support
+
+![Screenshot of sysinfobar using FontAwesome](doc/img/sysinfobar-fontawesome.png)
+
+See [how to compile](#With FontAwesome support) and [configuration below](#Example configuration for xmobar).
+
+### Simple view
+
+![Screenshot of simple sysinfobar](doc/img/sysinfobar-simple.png)
+
 ## Displayed information
 
 * CPU usage in percent
@@ -15,3 +27,55 @@ information to stdout in a format that is expected to be piped to
 
 The information is also highlighted according to the
 impact on the system.
+
+## How to build sysinfobar
+
+### With FontAwesome support
+
+```
+cargo build --features fontawesome
+```
+
+### Simple build
+
+```
+cargo build
+```
+
+## Example configuration for xmobar
+
+```
+Config {
+	font =         "xft:Fantasque Sans Mono:size=13:bold:antialias=true"
+	, additionalFonts = [ "xft:FontAwesome" ]
+	, bgColor =      "#202020"
+	, fgColor =      "orange"
+	, alpha = 255
+	, position =     TopW R 40
+	, border =       NoBorder
+	, borderColor =  "black"
+
+	, sepChar =  "%"
+	, alignSep = "}{"
+	, iconRoot = ""
+	, template = "}{ %UnsafeStdinReader%"
+
+	, textOffset = -1
+	, iconOffset = -1
+
+	, lowerOnStart =     True
+	, hideOnStart =      False
+	, allDesktops =      True
+	, overrideRedirect = True
+	, pickBroadest =     False
+	, persistent =       True
+
+	, commands = [ Run UnsafeStdinReader ]
+}
+```
+
+## How to use with xmobar
+
+```
+sysinfobar | xmobar -d sysinfo_xmobar.rc
+```
--- src/output/xmobar/mod.rs
+++ src/output/xmobar/mod.rs
@@ -20,6 +20,18 @@ impl Xmobar {
                 0..=100 => format!("<fc={}>|</fc> ", COLOR_INACTIVE),
                 _       => String::new(),
             };
+
+            #[cfg(feature = "fontawesome")]
+            let symb = match pow.battery_life {
+                0..=10   => format!("<fc={}><fn=1>\u{f244}</fn></fc> ", COLOR_CRITICAL),
+                11..=25  => format!("<fc={}><fn=1>\u{f243}</fn></fc> ", COLOR_WARNING),
+                26..=40  => format!("<fc={}><fn=1>\u{f242}</fn></fc> ", COLOR_WARNING),
+                41..=70  => format!("<fc={}><fn=1>\u{f241}</fn></fc> ", COLOR_MEDIUM),
+                71..=100 => format!("<fc={}><fn=1>\u{f240}</fn></fc> ", COLOR_NORMAL),
+                _        => String::new(),
+            };
+
+            #[cfg(not(feature = "fontawesome"))]
             let symb = match pow.battery_life {
                 0..=10   => format!("<fc={}>[ ! ]=</fc> ", COLOR_CRITICAL),
                 11..=25  => format!("<fc={}>['  ]=</fc> ", COLOR_WARNING),
@@ -30,6 +42,7 @@ impl Xmobar {
                 81..=100 => format!("<fc={}>[||']=</fc> ", COLOR_NORMAL),
                 _        => String::new(),
             };
+
             let left = match pow.ac_state {
                 0 => if pow.supports_time {
                     format!("{:3}min ", pow.minutes_left)
@@ -84,6 +97,29 @@ impl Xmobar {
 
 impl Output for Xmobar {
     fn refresh(status_data: &StatusData) {
+
+        #[cfg(feature = "fontawesome")]
+        println!("<fc={}><fn=1>\u{f142}</fn></fc>   <fc={}><fn=1>\u{f0e4}</fn> {:3}%</fc>   \
+                 <fc={}><fn=1>\u{f00a}</fn> {:3}%</fc>   <fc={}><fn=1>\u{f1c0}</fn> {:3}%</fc>   \
+                 <fc={}><fn=1>\u{f019}</fn> {:3}%</fc>   <fc={}><fn=1>\u{f093}</fn> {:3}%</fc>  {}  \
+                 <fc={}>{}</fc>",
+                 COLOR_INACTIVE,
+                 Xmobar::cpu_color(status_data.load, status_data.cpus),
+                 status_data.load,
+                 Xmobar::mem_color(status_data.memused),
+                 status_data.memused,
+                 Xmobar::swp_color(status_data.swpused),
+                 status_data.swpused,
+                 Xmobar::net_color(status_data.net_rx),
+                 status_data.net_rx,
+                 Xmobar::net_color(status_data.net_tx),
+                 status_data.net_tx,
+                 Xmobar::out_battery(&status_data.power_info),
+                 COLOR_ACTIVE,
+                 status_data.time
+                );
+
+        #[cfg(not(feature = "fontawesome"))]
         println!("<fc={}>|</fc> <fc={}>CPU:{:3}%</fc> <fc={}>|</fc> \
                  <fc={}>MEM:{:3}%</fc> <fc={}>SWP:{:3}%</fc> <fc={}>|</fc> \
                  <fc={}>RX:{:3}%</fc> <fc={}>TX:{:3}%</fc> {}<fc={}>|</fc> \
@@ -106,6 +142,7 @@ impl Output for Xmobar {
                  COLOR_ACTIVE,
                  status_data.time
                 );
+
         io::stdout().flush().unwrap();
     }
 }
