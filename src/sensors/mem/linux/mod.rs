47a910a16de7295d81d731f596b163a9604dcb97
nakal <nakal@users.noreply.github.com>
2023-09-21 14:19:50 +0200

    Add mem module support for Linux

--- src/sensors/mem/linux/mod.rs
+++ src/sensors/mem/linux/mod.rs
@@ -1,40 +1,72 @@
 extern crate libc;
 
-#[repr(C,packed)]
+use std::fs;
+
 pub struct MemInfo {
-    ignore1: [i32; 3],
-    npages: i32,
-    free: i32,
-    ignore2: [i32; 21],
-    swpages: i32,
-    ignore3: i32,
-    swpgonly: i32,
-    ignore4: [i32; 57],
+    mem_total: u64,
+    mem_free: u64,
+    swap_total: u64,
+    swap_free: u64,
 }
 
 impl MemInfo {
     fn new() -> MemInfo {
         MemInfo {
-            ignore1: [0; 3],
-            npages: 0,
-            free: 0,
-            ignore2: [0; 21],
-            swpages: 0,
-            ignore3: 0,
-            swpgonly: 0,
-            ignore4: [0; 57],
+            mem_total: 0,
+            mem_free: 0,
+            swap_total: 0,
+            swap_free: 0,
         }
     }
 
     pub fn fetch() -> MemInfo {
-        MemInfo::new()
+        let mut info = MemInfo::new();
+        let contents = fs::read_to_string("/proc/meminfo")
+                    .expect("Cannot access /proc/meminfo");
+        for line in contents.lines() {
+            let v: Vec<&str> = line.split_whitespace().collect();
+            if v[0].eq("MemTotal:") {
+                match v[1].parse::<u64>() {
+                    Ok(v) => info.mem_total = v,
+                    Err(_) => info.mem_total = 0,
+                };
+            }
+            if v[0].eq("MemFree:") {
+                match v[1].parse::<u64>() {
+                    Ok(v) => info.mem_free = v,
+                    Err(_) => info.mem_free = 0,
+                };
+            }
+            if v[0].eq("SwapTotal:") {
+                match v[1].parse::<u64>() {
+                    Ok(v) => info.swap_total = v,
+                    Err(_) => info.swap_total = 0,
+                };
+            }
+            if v[0].eq("SwapFree:") {
+                match v[1].parse::<u64>() {
+                    Ok(v) => info.swap_free = v,
+                    Err(_) => info.swap_free = 0,
+                };
+            }
+        }
+
+        info
     }
 
     pub fn memused(&self) -> u32 {
-        0
+        if self.mem_total == 0 {
+            0
+        } else {
+            (100 - ((self.mem_free * 100) / self.mem_total)) as u32
+        }
     }
 
     pub fn swpused(&self) -> u32 {
-        0
+        if self.swap_total == 0 {
+            0
+        } else {
+            (100 - ((self.swap_free * 100) / self.swap_total)) as u32
+        }
     }
 }
